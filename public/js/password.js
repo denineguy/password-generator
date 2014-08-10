c9d83406fb8ce1dec3921941b219115a3efb77df
Denine Guy <denineguy@gmail.com>
2014-08-10 15:53:58 -0400

    password validation working

--- public/js/password.js
+++ public/js/password.js
@@ -1,25 +1,42 @@
-$(function(){
-  $('#create').click(function(e){
-    e.preventDefault();
-    if($("#first_word").val() == '' {
-      alert("empty"); 
-      redirect_to("/password")
-    }
-    else if($("#second_word").val() == '']
-      alert("empty"); 
-      redirect_to("/password")
-    }
-    else if ($("#first_word").val().length <= 5){
-      alert("empty"); 
-      redirect_to("/password")
-    }
-    else if ($("#second_word").val().length <= 5){
-      alert("empty"); 
-      redirect_to("/password")
-    }
-    else{
-      return true;
-      redirect_to("/password")
-    }
-  })
-});
+
+$(document).ready(function(){
+  $('.error').hide();
+
+  $('#create').click(function(event){
+    event.preventDefault();   
+    validateForm();
+  }); 
+});      
+
+function validateForm() {
+  console.log("Validating form...");
+  $('.error').hide();
+  alert("clicked");
+
+  var fword = $('#first_word').val();
+  var sword = $('#second_word').val();
+
+  var haserrors = false;
+  if(fword.length<1) {
+    $('#first_word_error').show();
+    haserrors = true;
+    console.log('bad fword');
+  }
+
+  if(sword.length<1) {
+    $('#second_word_error').show();
+    haserrors = true;
+    console.log('bad sword');
+  }
+
+  if (!haserrors) {
+    submit_form(fword, sword);
+  }
+}
+
+function submit_form(fword, sword) {
+  console.log('Form submitted successfully.');
+  $('#display_data').append(
+    fword
+    +'<li>'+sword+'</li>');
+}
